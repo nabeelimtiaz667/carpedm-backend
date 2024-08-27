a2159c4a348adc9386dddb4c202adc1b0d0baba4
Nabeel Imtiaz <hitmanboy27@gmail.com>
2024-08-27 20:44:00 +0500

    Commented all the code for better help

--- functions.js
+++ functions.js
@@ -206,24 +206,52 @@ const updateDeparture = async (data = { departure_id: "", update: {} }) => {
     } catch (err) { console.error(err); }
 }
 
+// This is search page api user will search something and its data will be passed to this api
+// The result of this api will be displayed in form of tiles on results page
+fetchTours("kenya").then((res) => console.log(res.data.results));
 
-// getCustomer("5388132").then((res) => console.log(res));
-// fetchTours("kenya").then((res) => console.log(res.data.results));
-// getResources(RESOURCES.tour_dossiers, "25242").then((res) => console.log(res));
-// fetchDepartures("1274257").then((res) => console.log(res));
-// createBooking({ currency: "USD", externalId: "CARPADEM" }).then((res) => console.log(res));
-// createCustomer({
-//     "name": {
-//         "legal_first_name": "G",
-//         "legal_last_name": "Corp",
-//         "title": "Mr"
-//     },
-//     "date_of_birth": "1980-10-05",
-//     "account_email": "g.corp@gadventures.com",
-//     "nationality": {
-//         "id": "40",
-//     },
-// }).then((res) => console.log(res));
-// bookDeparture({ booking_id: "2212902", product_id: "1274257", customers: [{ id: "5449000" }] }).then((res) => console.log(res));
+// This api is for display tour details on next page. You can get id of a tour in the results json object -
+// - from previous page.
+// This function getResources() is also used to fetch other resources like countries, activities, etc.
+// Refer to RESOURCES variable above.
+getResources(RESOURCES.tour_dossiers, "25242").then((res) => console.log(res));
+
+// Once user is ready to book tour, they will be displayed all the available departures on this page.
+// User will select one of the departures for the booking and you will save the id for that departure
+fetchDepartures("1274257").then((res) => console.log(res));
+
+// Once a user selects a departure, their booking will be created. Data in this function will not change.
+// This booking will return a booking_id. 
+createBooking({ currency: "USD", externalId: "CARPADEM" }).then((res) => console.log(res));
+
+// Once customer selects no of travellers and input their details, you will send their data in this function.
+// For multiple traveller under one tour, this function will be called every single time for each customer -
+// - travelling. This will return customer_id for each and every customer.
+createCustomer({
+    "name": {
+        "legal_first_name": "G",
+        "legal_last_name": "Corp",
+        "title": "Mr"
+    },
+    "date_of_birth": "1980-10-05",
+    "account_email": "g.corp@gadventures.com",
+    "nationality": {
+        "id": "40",
+    },
+}).then((res) => console.log(res));
+
+// Finally, you will enter the booking_id, departure_id, and all the customer_id here. You can see customer -
+// - data is taken in array format, even if only one customer is present.
+bookDeparture({ booking_id: "2212902", product_id: "1274257", customers: [{ id: "5449000" }] }).then((res) => console.log(res));
+
+// Once a departure is booked, and once user has payed the fee, the admin will confirm the booking with -
+// - function
 updateDeparture({ departure_id: "1274257", update: { status: "Confirmed" } }).then((res) => console.log(res));
-// updateCustomer({ cust_id: "5388151", cust_data: { name: { legal_middle_name: "Doe" } } }).then((res) => console.log(res));
+
+// If a particular departure requires any more user information, you will get the object of remaining info -
+// - from bookDeparture() method, and you can fulfill those information by updating exisiting customer info -
+// - from this method, just input the customer_id
+updateCustomer({ cust_id: "5388151", cust_data: { name: { legal_middle_name: "Doe" } } }).then((res) => console.log(res));
+
+// If you want to see complete info for a particular customer you can see by giving customer id here
+getCustomer("5388132").then((res) => console.log(res));
